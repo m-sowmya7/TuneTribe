028776b62d5ea8d2c33244d0eaa0fb1f4b5b512f
Sowmya <134478167+m-sowmya7@users.noreply.github.com>
2025-08-20 07:13:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
 import AudioPlayer from "./pages/AudioPlayer";
 import { MusicProvider } from "./context/MusicProvider";
+import MiniPlayer from "./components/player/MiniPlayer";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import SearchPage from "./pages/SearchPage";
 import AlbumPage from "./pages/AlbumPage";
 import { ReactLenis } from "lenis/react";
+
+// Component to conditionally render MiniPlayer based on route
+function ConditionalMiniPlayer() {
+  const location = useLocation();
+
+  // Routes where MiniPlayer should NOT be shown
+  const excludedRoutes = ["/", "/player"];
+
+  // Check if current route should exclude MiniPlayer
+  const shouldHideMiniPlayer = excludedRoutes.some((route) => {
+    if (route === "/") {
+      return location.pathname === "/";
+    }
+    if (route === "/player") {
+      return location.pathname.startsWith("/player/");
+    }
+    return false;
+  });
+
+  // Don't render MiniPlayer on excluded routes
+  if (shouldHideMiniPlayer) {
+    return null;
+  }
+
+  return <MiniPlayer />;
+}
 
 function App() {
   const lenisOptions = {
@@ -33,6 +65,7 @@ function App() {
               <Route path="/search/:id" element={<SearchPage />} />
               <Route path="/album/:id" element={<AlbumPage />} />
             </Routes>
+            <ConditionalMiniPlayer />
           </Router>
         </ReactLenis>
       </MusicProvider>
